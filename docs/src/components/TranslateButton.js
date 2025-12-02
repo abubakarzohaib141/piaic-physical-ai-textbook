f28d42b030d8dabea5135b4b6955d67683ade699
Abu Bakar <abubakarbinzohaib@gmail.com>
2025-12-02 18:55:50 +0500

    Update TranslateButton.js

--- docs/src/components/TranslateButton.js
+++ docs/src/components/TranslateButton.js
@@ -6,7 +6,7 @@ export default function TranslateButton({ content, onTranslated }) {
   const [isUrdu, setIsUrdu] = useState(false);
   const [originalContent] = useState(content);
 
-  const API_URL = 'http://localhost:8000';
+  const API_URL = 'https://backend-piaic-hackathon-2.onrender.com';
 
   const handleToggleTranslation = async () => {
     if (isUrdu) {
